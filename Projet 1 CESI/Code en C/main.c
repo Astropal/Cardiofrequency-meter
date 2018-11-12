b9b677389cc940ef9cfb8b4c0a721f302a9c2a62
Clément <albosclement@gmail.com>
2018-11-12 15:54:34 +0100

    Module 4

---
+++ Projet 1 CESI/Code en C/main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+  int choice = 1;
+
+      printf("**** INTERFACE DE CONTROLE ****\n\n");
+
+    while ((choice > 0) && (choice < 7))
+    {
+          printf("Que souhaitez-vous faire?\n\n");
+          printf("(1)     Afficher les donnees du fichier\n");
+          printf("(2)     Afficher les donnees du dossier par ordre croissant/decroissant\n");
+          printf("(3)     Rechercher et afficher une donnee a un temps particulier\n");
+          printf("(4)     Afficher la moyenne de pouls dans une plage de temps donnée\n");
+          printf("(5)     Afficher le nombre de lignes de données actuellement en mémoire\n");
+          printf("(6)     Rechercher et afficher les max/min de pouls (avec le temps associé)\n");
+          printf("(autre) Quitter?\n\n");
+
+          scanf("%d",&choice);
+
+          switch(choice)
+  		    {
+        			case 1 :
+        			{
+
+        				break;
+        			}
+        			case 2  :
+        			{
+
+        				break;
+        			}
+        			case 3 :
+              {
+
+                break;
+              }
+              case 4 :
+              {
+
+                break;
+              }
+              case 5 :
+              {
+
+                break;
+              }
+              case 6 :
+              {
+
+                break;
+              }
+
+        			default :
+        			{
+        				printf("Fermeture du programme");
+        			}
+  		}
+    }
+
+
+
+}
