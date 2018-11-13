869879108b16c8ea083350a9f7a6f4935a5240e1
Astropal <bastien.dupont@viacesi.fr>
2018-11-13 10:59:55 +0100

    Suppression du vieux module 4

--- Module test/action.h
+++
@@ -1,8 +0,0 @@
-#ifndef ACTION_H_INCLUDED
-#define ACTION_H_INCLUDED
-#include "donnees.h"
-
-void afficherDonnee(donnee *maDonne);
-void afficherToutesDonnees(donnee *listeDonnee);
-
-#endif // ACTION_H_INCLUDED
--- Module test/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "menu.h"
-
-void main()
-{
-    afficherMenu();
-}
